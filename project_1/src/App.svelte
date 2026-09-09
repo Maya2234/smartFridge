97637a7eb7e9fb13e95047247dda26f5c68e8187
Maya <mayato223@gmail.com>
2026-09-09 14:03:45 -0400

    initali commit

---
+++ project_1/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from './assets/vite.svg'
+  import heroImg from './assets/hero.png'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<section id="center">
+  <div class="hero">
+  </div>
+  <div>
+    <h1>Project 1: Smart Fridge</h1>
+    <p>Maya Tomarchio</p>
+  </div>
+  <!--<Counter />-->
+</section>
+
+<div class="ticks"></div>
+
+<section id="next-steps">
+  <div id="docs">
+    <svg class="icon" role="presentation" aria-hidden="true">
+      <use href="/icons.svg#documentation-icon"></use>
+    </svg>
+    <h2>Prototype</h2>
+    <p></p>
+    <ul>
+      <li>
+      </li>
+      <li>
+
+      </li>
+    </ul>
+  </div>
+
+</section>
+
+<div class="ticks"></div>
+<section id="spacer"></section>
